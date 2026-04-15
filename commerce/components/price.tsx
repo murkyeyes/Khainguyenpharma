c0f6a073744fc2f449350c2309c3432c74203bd5
murkyeyes <khai.phamkhaipham@hcmut.edu.vn>
2026-04-16 01:04:11 +0700

    fix: rewrite Price to strictly client component to completely bypass hydration #418

--- commerce/components/price.tsx
+++ commerce/components/price.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 
 const Price = ({
   amount,
@@ -10,19 +13,31 @@ const Price = ({
   className?: string;
   currencyCode: string;
   currencyCodeClassName?: string;
-} & React.ComponentProps<"span">) => (
-  <span suppressHydrationWarning={true} className={className}>
-    <span suppressHydrationWarning className={className}>
-    {`${new Intl.NumberFormat('vi-VN', {
-      style: "currency",
-      currency: currencyCode,
-      currencyDisplay: "narrowSymbol",
-    }).format(parseFloat(amount))}`}
+} & React.ComponentProps<"span">) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <span className={className}>...</span>;
+  }
+
+  return (
+    <span className={className}>
+      <span className={className}>
+        {`${new Intl.NumberFormat("vi-VN", {
+          style: "currency",
+          currency: currencyCode,
+          currencyDisplay: "narrowSymbol",
+        }).format(parseFloat(amount))}`}
+      </span>
+      <span className={clsx("ml-1 inline", currencyCodeClassName)}>
+        {`${currencyCode}`}
+      </span>
     </span>
-    <span
-      className={clsx("ml-1 inline", currencyCodeClassName)}
-    >{`${currencyCode}`}</span>
-  </span>
-);
+  );
+};
 
 export default Price;
