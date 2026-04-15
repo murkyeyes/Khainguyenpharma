9b35783fd1d7da1f6c5239f67b50ef590025e2c1
murkyeyes <khai.phamkhaipham@hcmut.edu.vn>
2026-04-16 00:33:23 +0700

    fix: suppress hydration warning for dynamic date in footer

--- commerce/components/layout/footer.tsx
+++ commerce/components/layout/footer.tsx
@@ -57,7 +57,7 @@ export default async function Footer() {
       </div>
       <div className="border-t border-neutral-200 py-6 text-sm">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          <p>
+          <p suppressHydrationWarning>
             &copy; {copyrightDate} {copyrightName}
             {copyrightName.length && !copyrightName.endsWith(".")
               ? "."
